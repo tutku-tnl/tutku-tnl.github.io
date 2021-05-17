88aefc992483cf4b3465c244fb790d2fe29fd33a
tutku-tnl <83888982+tutku-tnl@users.noreply.github.com>
2021-05-17 15:40:00 +0300

    Update lastIAT.js

--- lastIAT.js
+++ lastIAT.js
@@ -39,7 +39,7 @@ define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(A
 		},	
 
 		base_url : {//Where are your images at?
-			image : 'https://baranan.github.io/minno-tasks/images/'
+			image : 'https://tutku-tnl.github.io/iatcode/'
 		} 
 	});
 });
